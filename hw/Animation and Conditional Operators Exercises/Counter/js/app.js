b1c84e303195267ec787e46a212e024495fa446b
isdahami <isdahami@iu.edu>
2022-01-24 10:43:25 -0500

    Create app.js

---
+++ hw/Animation and Conditional Operators Exercises/Counter/js/app.js
@@ -0,0 +1,15 @@
+// Isaac Hamilton
+// 1/18/2022
+//n220
+
+
+// starts the "counter" at 0
+let counter = 0;
+
+// creates the function draw
+function draw() {
+    // draws the function to add 1 over and over again
+    counter = counter + 1;
+    // prints JS into the console, in this instance you will see numbers going up
+    console.log(counter);
+}
