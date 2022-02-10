b97b7076dc4fffe43652f3b534ab8353ca53c36a
isdahami <isdahami@iu.edu>
2022-02-09 20:33:51 -0500

    Update app.js

--- hw/Objects Exercises/Data-driven display/js/app.js
+++ hw/Objects Exercises/Data-driven display/js/app.js
@@ -7,26 +7,31 @@
 // Then, create a function that takes one argument and draws an object to the screen using that argument. Pass your object to that function as an argument, and have the function draw the object to the screen.
 
 
-
+// create an object with properties
 let someObj = {
     height: 25,
     width: 60,
     color: "#03fcd3"
 }; 
 
+// create the function setup with canvas and background
 function setup() {
     createCanvas(400, 600);
     background(100);
 }
 
+// draw function
 function draw() {
+    // fills the obj with the color prop I selected
     fill(someObj.color);
     background(100);
-    //rect(mouseX, mouseY, someObj.height, someObj.width);
+    // calls the drawObj function with the argument of the object
     drawObj(someObj);
 }
 
+// drawObj function with the argument obj
 function drawObj(obj) {
+    // creates a rect at mouseX, mouseY, with the obj height, and obj width
     rect(mouseX, mouseY, obj.height, obj.width);
 
 }
