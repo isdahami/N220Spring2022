9469fd913897c621044a7758e2039596fbd685fd
isdahami <isdahami@iu.edu>
2022-02-09 20:33:49 -0500

    Update app.js

--- hw/Objects Exercises/Virtual Pet/js/app.js
+++ hw/Objects Exercises/Virtual Pet/js/app.js
@@ -14,6 +14,8 @@
 
 // ( use if(mousePressed) { to check if the mouse is down. )
 
+
+// Created an object name someObj
 let someObj = {
     x: 20,
     y: 40,
@@ -21,26 +23,34 @@ let someObj = {
     offsetX: 3
 }
 
+// create the function setup
 function setup() {
     createCanvas(400, 800);
     background(100);
 }
 
+// created the draw function
 function draw() {
     background(100);
+    // sets the Object x so that it is equal to mouseX
     someObj.x = mouseX
+    // sets the Object y so that it is equal to mouseY
     someObj.y = mouseY 
+    // calls the offset function
     offset();
+    // draws the circle, the Obj.x is being added to the someObj.offsetX
     circle((someObj.x + someObj.offsetX), someObj.y, someObj.size);
 }
 
+// this is the offset function
 function offset() {
+    // if the mouse is pressed,
     if(mouseIsPressed == true) {
-        
+        // the offsetX is not equal to 0 it will subtract one
         if(someObj.offsetX != 0) {
             someObj.offsetX = someObj.offsetX - 1
         } 
-    } else {
+    } else { // else the offsetX will add 1
         someObj.offsetX = someObj.offsetX + 1
     }
 }
