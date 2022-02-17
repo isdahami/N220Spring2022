685ae4d397de26d6b62494d98d9f2122fc0c51ff
isdahami <isdahami@iu.edu>
2022-02-16 21:27:09 -0500

    Update app.js

--- hw/Document Object Manipulation Exercises/Peak Pixels/js/app.js
+++ hw/Document Object Manipulation Exercises/Peak Pixels/js/app.js
@@ -11,21 +11,27 @@ Make a variable to store the height and width of the object
 to set the height and width, set to varName + "px"
 10% is .1 bigger,or 1.1 * the original size */ 
 
-
+// creates the box
 let dvBox = document.getElementById("box");
 
+// styling the box
 dvBox.style.backgroundColor = "green"
 dvBox.style.width = 100 + "px";
 dvBox.style.height = 100 + "px";
 
+// places the element on the page
 document.body.appendChild(dvBox);
+// setting the variables
     var boxHeight = dvBox.style.height;
     var boxWidth = dvBox.style.width;
 
+// function that increases the box size
 function increaseBox() {
     boxHeight = parseInt(boxHeight) * 1.1 + "px";
     boxWidth = parseInt(boxWidth) * 1.1 + "px";
     dvBox.style.height = boxHeight;
     dvBox.style.width = boxWidth;
+
+    // puts the element on the page
     document.body.appendChild(dvBox);
 }
