01e6a3ffec41f522044e096763e8bee67e7dc84a
isdahami <isdahami@iu.edu>
2022-02-16 21:27:07 -0500

    Update app.js

--- hw/Document Object Manipulation Exercises/Over and Out/js/app.js
+++ hw/Document Object Manipulation Exercises/Over and Out/js/app.js
@@ -4,9 +4,10 @@
 
 /* Write the markup and JavaScript to place a square div on the page (100px x 100px), with a blue background. Using onmouseover and onmouseout (instead of "onclick"), change the div's color to black when the mouse is over the div, and back to blue when the mouse leaves. */ 
 
-
+// creating the box
 let dvBox = document.getElementById("box");
 
+// styling 
 dvBox.style.width = "100px";
 dvBox.style.height = "100px";
 
@@ -15,10 +16,12 @@ dvBox.style.height = "100px";
 document.getElementById("box").onmouseover = function() {mouseOver()};
 document.getElementById("box").onmouseout = function() {mouseOut()};
 
+// function that happens when the mouse is over the box, changes it to black
 function mouseOver() {
      document.getElementById("box").style.backgroundColor = "black";
  }
 
+ // function that happens when the mouse is not over the box, changes it to blue
 function mouseOut() {
     document.getElementById("box").style.backgroundColor = "blue";
  }
