0cba40bc8b33e847d12c32d2ac2caf885a84ce43
isdahami <isdahami@iu.edu>
2022-02-17 15:02:36 -0500

    Update app.js

--- hw/Document Object Manipulation Exercises/McDiv'ns/js/app.js
+++ hw/Document Object Manipulation Exercises/McDiv'ns/js/app.js
@@ -5,25 +5,14 @@
 /* Put a div on the page. When the div is clicked, append the text "mc" to whatever is in its innerHTML. After 3 clicks, the div will show "divdivdiv" */
 
 // creating the box
-let dvBox = document.getElementById("box");
+let newEl = document.createElement("div");
 
 // styling 
-dvBox.innerHTML = "mc";
-dvBox.style.backgroundColor = "#ebcf34";
-dvBox.style.width = "100px";
-dvBox.style.height = "100px";
+newEl.innerHTML = "mc";
 
-// function newBox
-function newBox(){
-    // creating a new element 
-    let newEl = document.createElement("div");
-    
-    // styling the new element
-    newEl.innerHTML = "div";
-    newEl.style.backgroundColor = "green"
-    newEl.style.width = "50px";
-    newEl.style.float = "left";
 
-    // putting it on the page
-    document.body.appendChild(newEl);
-}
+document.body.appendChild(newEl);
+
+newEl.addEventListener("click", function() {
+    newEl.innerHTML += "div";
+})
