edf8cdc56bd6e32e3a54d9622103912a21256f08
isdahami <isdahami@iu.edu>
2022-02-24 17:15:06 -0500

    Update app.js

--- hw/Loops and Arrays/Beep Bop/js/app.js
+++ hw/Loops and Arrays/Beep Bop/js/app.js
@@ -10,10 +10,32 @@ Write a loop that runs 25 times and writes the iterand (current iteration of the
 
 */
 
+// create variable numCounter, get the element by id
 let numCounter = document.getElementById("numCounter");
 
-for(let i = 0; i < 26; i++) {
-    numCounter.innerHTML += i;
+// begin the for loop to generate 1 - 25
+for(let i = 1; i < 26; i++) {
     console.log(i)
+    // begin an if statement, i is divided by 3 and i is divided by 5.
+    if((i % 3 == 0) && (i % 5 == 0)){
+        // prints out beepbop if true
+        numCounter.innerHTML += "beepbop" + "<br/>";
+    }
+
+    // begin an else if, i is divided by 3
+    else if(i % 3 == 0) {
+        // prints out beep if true
+        numCounter.innerHTML += "beep" + "<br/>";
+    }
+
+    // begin an else if, i is divided by 5 if true.
+    else if(i % 5 ==0) {
+        // prints bop if true
+        numCounter.innerHTML += "bop" + "<br/>";
+    } else {
+        numCounter.innerHTML += i + "<br/>";
+    }
+
+ 
 }
 
