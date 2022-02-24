138c2e1497dde51277008e8d5f484dff3df0fbe4
isdahami <isdahami@iu.edu>
2022-02-24 17:14:57 -0500

    Update app.js

--- hw/Loops and Arrays/Runner Up/js/app.js
+++ hw/Loops and Arrays/Runner Up/js/app.js
@@ -10,11 +10,18 @@ Then, write the line of code that displays the runner-up times (second and third
 
 */
 
+
+// create array of best times
 let bestTimes = [3.87 , 9.34 , 16.77 ]
 
+
+// begin for loop, start at 0, i is less than the length of best times, adds one
 for(i = 0; i < bestTimes.length; i++) {
+    // get the element id bestTimesZero
     let bestTimesZero = document.getElementById("bestTimesZero");
+    // create statement, pulls bestTime 0 from the array and adds the string to the end
     let statement = bestTimes[0] + " 1st";
+    // prints it onto the page
     bestTimesZero.innerHTML = statement;
 }
 
