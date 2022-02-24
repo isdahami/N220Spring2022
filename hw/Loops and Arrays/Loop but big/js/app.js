f8fd5cbd3ef8915cba809ceb7608cb2faa9385c9
isdahami <isdahami@iu.edu>
2022-02-24 17:15:00 -0500

    Update app.js

--- hw/Loops and Arrays/Loop but big/js/app.js
+++ hw/Loops and Arrays/Loop but big/js/app.js
@@ -8,21 +8,20 @@ n220
 Write a loop that displays in a div the numbers 1000, 2000, 3000, 4000, 5000. There should only be one line of code inside of your loop.
 
 
-let num = [1000, 2000, 3000, 4000, 5000]
-
-for(let i = 0; i <,5; i++) {
-    console.log(i);
-}
-
 */ 
 
+
+// create array
 let num = [1000, 2000, 3000, 4000, 5000]
 
+// begin the for loop, start at 0, i is less than 5001, adds one until 5001
 for(let i = 0; i < 5001; i++) {
     console.log(i);
 }
 
-
+// gets element "numCounter"
 let numCounter = document.getElementById("numCounter");
+// create statement, num
 let statement = num;
+// prints out the result onto the page
 numCounter.innerHTML = statement;
